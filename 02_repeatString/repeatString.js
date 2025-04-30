2a3b3aba53175c0e995b796ef62a65cafe195d5b
Tim <tim.duke.314@gmail.com>
2025-04-30 10:36:38 -0500

    Finished 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str1, rep1) {
+  if (rep1 < 0) {
+    return "ERROR";
+  }
+  let string1 = str1;
+  let string2 = "";
+  for (let i = 0;i < rep1;i++) {
+    string2 = string2 + string1;
+  }
+  return string2;
 };
 
 // Do not edit below this line
