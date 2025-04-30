d025aa46c4f40aab08e22026cb160ba99b7e4fd1
Tim <tim.duke.314@gmail.com>
2025-04-30 11:24:22 -0500

    Finished 04-removeFromArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+  let filteredArray = array;
+  let i = 0;
+  while (remove[i]) {
+    for (let j=0;j<filteredArray.length;j++) {
+      if (filteredArray[j] === remove[i]) {
+        filteredArray.splice(j, 1);
+        j--
+      }
+    }
+    i++
+  }
+  return filteredArray;
 };
 
 // Do not edit below this line
