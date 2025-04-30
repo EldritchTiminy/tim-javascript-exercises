ab9de60b15083784fdf354e1df9a176595023bcf
Tim <tim.duke.314@gmail.com>
2025-04-30 10:49:06 -0500

    Finished 03_reverseString.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let forword = word;
+  let reversed = "";
+  for (let i=0;i<forword.length;i++) {
+    reversed += forword[forword.length - 1 - i];
+  }
+  return reversed;
 };
 
 // Do not edit below this line
