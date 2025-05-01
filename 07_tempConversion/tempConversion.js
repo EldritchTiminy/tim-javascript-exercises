cf1d5a61c8f5dd8667c73abc886ec9abbfc2ddaa
Tim <tim.duke.314@gmail.com>
2025-05-01 08:30:14 -0500

    Finished 07_tempConversion.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(ftemp) {
+  let ctemp = Math.round(((ftemp - 32) * (5 / 9)) * 10) / 10;
+  return ctemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(ctemp) {
+  let ftemp = Math.round(((ctemp * (9 / 5)) + 32) * 10) / 10;
+  return ftemp;
 };
 
 // Do not edit below this line
