18b790ee45abb8e08f12cd8bba48ec485f29c53e
Tim <tim.duke.314@gmail.com>
2025-05-23 12:41:17 -0500

    finished 10 fibonacci.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(in1) {
+  let fibSeq = [0];
+  let sequencer = 1;
+  if (in1 < 0) {
+    return "OOPS";
+  } else {
+    for (let counter = 0; counter < in1; counter++) {
+      fibSeq.push(sequencer);
+      sequencer += fibSeq[counter];
+    };
+    return fibSeq[in1];
+  };
 };
 
 // Do not edit below this line
