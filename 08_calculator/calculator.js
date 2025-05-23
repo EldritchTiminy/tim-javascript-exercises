a2fa59904ba4e66036feda7ea278118422e0161a
Tim <tim.duke.314@gmail.com>
2025-05-23 12:24:39 -0500

    Finished 08 calculator and 09 palindromes.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(in1, in2) {
+	return (in1 + in2);
 };
 
-const subtract = function() {
-	
+const subtract = function(in1, in2) {
+	return (in1 - in2);
 };
 
-const sum = function() {
-	
+const sum = function(ar1) {
+  let sum = 0;
+	for (const number of ar1) {
+    sum += number;
+  };
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(ar1) {
+  let product = 1;
+  for (const number of ar1) {
+    product *= number;
+  };
+  return product;
 };
 
-const power = function() {
-	
+const power = function(in1, in2) {
+	let product = in1;
+  for (let counter = 1; counter < in2; counter++) {
+    product *= in1;
+  };
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(in1) {
+  let product = in1;
+  let multiplier = in1;
+  if (product <= 1) {
+    return 1;
+  } else {
+    while (multiplier > 1) {
+      multiplier -= 1;
+      product *= multiplier;
+    }
+    return product;
+  };
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str1) {
+  let filteredString = str1.toLowerCase().replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  let flipString = filteredString.split('').reverse().join('');
+  if (flipString === filteredString) {
+    return true;
+  } else {
+    return false;
+  };
 };
 
 // Do not edit below this line
