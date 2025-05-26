0336210be04eb4a7441cb630e350d93c023c7962
Tim <tim.duke.314@gmail.com>
2025-05-26 08:08:15 -0500

    Finished 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr1) {
+  let titles = arr1.map((object) => object.title);
+  return titles;
 };
 
 // Do not edit below this line
