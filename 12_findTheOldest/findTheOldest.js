9dc1bf493e87c2aaea1ccb601b725b731d78bc57
Tim <tim.duke.314@gmail.com>
2025-05-28 11:18:55 -0500

    Finished 12_findTheOldest.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr1) {
+  let oldest = arr1[0];
+  let objAge;
+  let oldestAge;
+  let date = new Date();
+  let currentYear = date.getFullYear();
+  if (oldest.yearOfBirth && oldest.yearOfDeath) {
+    oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+  } else if (oldest.yearOfBirth) {
+    oldestAge = currentYear - oldest.yearOfBirth;
+  } else {
+    console.log("Error: Insufficient Data");
+  };
 
+  for (let obj of arr1) {
+    if (obj.yearOfBirth && obj.yearOfDeath) {
+      objAge = obj.yearOfDeath - obj.yearOfBirth;
+    } else if (obj.yearOfBirth) {
+      objAge = currentYear - obj.yearOfBirth;
+    } else {
+      console.log("Error: Insufficient Data");
+    };
+    if (objAge > oldestAge) {
+      oldest = obj;
+      oldestAge = objAge;
+    }
+  };
+  return oldest;
 };
 
 // Do not edit below this line
